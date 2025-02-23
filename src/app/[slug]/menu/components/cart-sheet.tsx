031bc3b1d55a6a4f32a8ebb9cdd91d0c639c5e14
GuiLSDias <glsd273@gmail.com>
2025-02-22 22:44:20 -0300

    feat: final

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
@@ -13,8 +12,10 @@ import CartProductItem from "./cart-product-item";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { formatCurrency } from "@/helpers/format-currency";
+import FinishOrderDialog from "./finish-order-dialog";
 
 const CartSheet = () => {
+  const [finishOrderDialogIsOpen, setFinishOrderDialogIsOpen] = useState(false);
   const { isOpen, toggleCart, products, total } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
@@ -22,21 +23,30 @@ const CartSheet = () => {
         <SheetHeader>
           <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
-        <div className="flex flex-col h-full py-5">
-        <div className="flex-auto">
-        {products.map((product) => (
-          <CartProductItem key={product.id} product={product}/>
-        ))}
-        </div>
-        <Card className="mb-6">
-          <CardContent className="p-5">
-            <div className="flex justify-between">
-              <p className="text-sm text-muted-foreground">Total</p>
-              <p className="font-semibold text-sm">{formatCurrency(total)}</p>
-            </div>
-          </CardContent>
-        </Card>
-        <Button className="w-full rounded-full">Finalizar o pedido</Button>
+        <div className="flex h-full flex-col py-5">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CartProductItem key={product.id} product={product} />
+            ))}
+          </div>
+          <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="text-sm font-semibold">{formatCurrency(total)}</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Button
+            className="w-full rounded-full"
+            onClick={() => setFinishOrderDialogIsOpen(true)}
+          >
+            Finalizar pedido
+          </Button>
+          <FinishOrderDialog
+            open={finishOrderDialogIsOpen}
+            onOpenChange={setFinishOrderDialogIsOpen}
+          />
         </div>
       </SheetContent>
     </Sheet>
